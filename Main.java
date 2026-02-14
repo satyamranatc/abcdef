72063e9717e5c3018feca88ea86580ae4fa63443
Satyam Rana <satyamranatc@gmail.com>
2026-02-14 16:54:40 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello Java!");
+        System.out.println("Hello Devloper!");
+        System.out.println("Exp F1!");
+    }
+}
